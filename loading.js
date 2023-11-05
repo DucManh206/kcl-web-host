43e8025a39bed446cf665b8aceed84bb319b0c3b
DucManh206 <117666678+DucManh206@users.noreply.github.com>
2023-11-05 23:31:12 +0700

    update screen loading

---
+++ loading.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate loading time
+    setTimeout(function() {
+        // Redirect to the main page
+        window.location.href = "main.html";
+    }, 3000); // 3000 milliseconds (3 seconds) as an example
+});
