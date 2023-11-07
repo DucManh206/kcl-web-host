4a02a544e7517699a6fb21e478cd1056f9a40b9d
DucManh206 <117666678+DucManh206@users.noreply.github.com>
2023-11-07 22:47:57 +0700

    Tối ưu script.js

--- main/script.js
+++ main/script.js
@@ -1,91 +1,28 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
-    // Kiểm tra trạng thái chế độ từ Local Storage
+    // Check and apply dark mode from Local Storage
     var isDarkMode = localStorage.getItem("darkMode") === "true";
-
-    // Áp dụng chế độ tối nếu cần
     if (isDarkMode) {
         document.body.classList.add("dark-mode");
     }
 
-    // Cập nhật hình ảnh trong chế độ tối
+    // Update image colors for dark mode
     updateImageColors();
 
-    // Chắc chắn rằng phần tử có id là 'toggleMode' đã được tìm thấy
-    const toggleModeButton = document.getElementById('toggleMode');
-    if (toggleModeButton) {
-        toggleModeButton.addEventListener('click', function() {
-            toggleDarkMode();
-        });
-    }
-});
-
-function toggleDarkMode() {
-    // Đảo ngược trạng thái chế độ tối
-    document.body.classList.toggle("dark-mode");
-
-    // Lưu trạng thái chế độ vào Local Storage
-    var isDarkMode = document.body.classList.contains("dark-mode");
-    localStorage.setItem("darkMode", isDarkMode.toString());
-
-    // Cập nhật hình ảnh trong chế độ tối
-    updateImageColors();
-}
-
-function updateImageColors() {
-    // Cập nhật màu sắc của các hình ảnh dựa trên chế độ tối
-    var images = document.querySelectorAll("img");
-    images.forEach(function (img) {
-        var src = img.src;
-        if (document.body.classList.contains("dark-mode")) {
-            // Thay thế hình ảnh bằng phiên bản chế độ tối (nếu có)
-            src = src.replace(".png", "-dark.png");
-        } else {
-            // Thay thế hình ảnh bằng phiên bản chế độ sáng (nếu có)
-            src = src.replace("-dark.png", ".png");
-        }
-        img.src = src;
-    });
-}
-document.addEventListener("DOMContentLoaded", function () {
-    // Kiểm tra chiều rộng màn hình khi trang web được tải
-    checkScreenWidth();
-
-    // Thêm sự kiện lắng nghe cho việc thay đổi kích thước màn hình
-    window.addEventListener("resize", checkScreenWidth);
-});
-
-function checkScreenWidth() {
-    var isMobile = window.innerWidth <= 768;
-
-    // Lấy đối tượng body
-    var body = document.body;
-
-    // Thêm hoặc xóa lớp tùy thuộc vào loại thiết bị
-    if (isMobile) {
-        body.classList.add("mobile-view");
-        body.classList.remove("desktop-view");
-    } else {
-        body.classList.remove("mobile-view");
-        body.classList.add("desktop-view");
-    }
-}
-// Your existing JavaScript code here
-
-// Add these lines to the end of script.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Check if the element with id 'toggleMode' is found
+    // Ensure the element with id 'toggleMode' is found
     const toggleModeButton = document.getElementById('toggleMode');
     if (toggleModeButton) {
         toggleModeButton.addEventListener('click', function () {
             toggleDarkMode();
         });
     }
-});
 
+    // Check screen width on page load
+    checkScreenWidth();
 
-document.addEventListener("DOMContentLoaded", function () {
+    // Add event listener for screen width changes
+    window.addEventListener("resize", checkScreenWidth);
+
+    // Type text effect
     const typingText = document.getElementById("typing-text");
     const learnMoreBtnContainer = document.getElementById("learn-more-btn-container");
 
@@ -98,21 +35,56 @@ document.addEventListener("DOMContentLoaded", function () {
             index++;
             setTimeout(typeText, 36);
         } else {
-            // Hiển thị nút "Learn More" khi chữ đã gõ xong
+            // Show "Learn More" button when text is typed
             learnMoreBtnContainer.style.opacity = 1;
         }
     }
 
-    // Bắt đầu gõ chữ khi trang web được tải
+    // Start typing when the webpage is loaded
     typeText();
 });
 
-function handleLearnMoreClick() {
-    // Lấy đối tượng HTML của phần content
-    const contentSection = document.getElementById("content");
+function toggleDarkMode() {
+    // Toggle dark mode
+    document.body.classList.toggle("dark-mode");
 
-    // Sử dụng smooth-scroll để cuộn màn hình một cách từ từ
+    // Save dark mode status to Local Storage
+    var isDarkMode = document.body.classList.contains("dark-mode");
+    localStorage.setItem("darkMode", isDarkMode.toString());
+
+    // Update image colors for dark mode
+    updateImageColors();
+}
+
+function updateImageColors() {
+    // Update image colors based on dark mode
+    var images = document.querySelectorAll("img");
+    images.forEach(function (img) {
+        var src = img.src;
+        if (document.body.classList.contains("dark-mode")) {
+            src = src.replace(".png", "-dark.png");
+        } else {
+            src = src.replace("-dark.png", ".png");
+        }
+        img.src = src;
+    });
+}
+
+function checkScreenWidth() {
+    var isMobile = window.innerWidth <= 768;
+    var body = document.body;
+
+    if (isMobile) {
+        body.classList.add("mobile-view");
+        body.classList.remove("desktop-view");
+    } else {
+        body.classList.remove("mobile-view");
+        body.classList.add("desktop-view");
+    }
+}
+
+function handleLearnMoreClick() {
+    const contentSection = document.getElementById("content");
     const scroll = new SmoothScroll();
     scroll.animateScroll(contentSection, null, { easing: 'easeInOutQuart', speed: 2800 });
 }
-
